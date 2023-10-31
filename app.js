c9dbee601d4f62505d1ec7c7bb467bd5dcd15b5c
mariaSmartDesign <creativitatidisseny@gmail.com>
2023-10-31 10:48:12 +0100

    done

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // ¡Buena suerte!
+const button = document.querySelector("#get-advice-button");
+const quote = document.querySelector("#card-quote");
+const advice = document.querySelector("#advice-number");
+
+async function updateNewAdvice() {
+
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const randomAdvice = await response.json();
+
+    // console.log("Nuevo consejo!", randomAdvice);
+
+    quote.textContent = randomAdvice.slip.advice;
+    advice.textContent = randomAdvice.slip.id;
+}
+
+button.addEventListener("click", updateNewAdvice);
